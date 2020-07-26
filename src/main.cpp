c7b79302278ad58dfebd673d211b609dfd5c2dad
unknown <912020291@qq.com>
2020-07-26 19:23:14 +0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -19,24 +19,22 @@ int main(){
 
 	printf("\n");
 
-	char *a = new char[5];
 	char **b = new char*[3];
 	for (int i = 1; i < 3; i++){
-		*b = new char[5];
+		b[i] = new char[5];
 	}
 	for (int j = 1; j < 3; j++){
 		memcpy(b[j], str, sizeof(str));
 	}
 
-
 	printf("初始化后的字符数组:");
 
 	for(int i=0;i<5;i++)
-
 		printf("%s ",b[i]);
-	
-	delete []a;
-	a = NULL;
+
+	for(int k = 1; k<3; k++)
+		delete [] b[k];
+	delete []b;
 
 		
 
